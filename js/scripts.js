e140cf1a83d5e1b1635965d2e9033539ddcfc036
William Kinzig <wippawill2@gmail.com>
2018-05-30 19:15:01 -0700

    create add, subtract, multiply, and divide functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,23 @@ var add = function(num1,num2) {
 	return num1 + num2;
 }
 
+var subtract = function(num1,num2) {
+	return num1 - num2;
+}
+var divide = function(num1,num2) {
+	return num1 / num2;
+}
+var multiply = function(num1,num2) {
+	return num1 * num2;
+}
 var number1 = parseInt(prompt("Enter first number"));
 var number2 = parseInt(prompt("Enter second number"));
 
+
 alert(add(number1,number2));
+
+alert(subtract(number1,number2));
+
+alert(multiply(number1,number2));
+
+alert(divide(number1,number2));
