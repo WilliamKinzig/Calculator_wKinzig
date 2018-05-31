26e70a81c3016e9c17943f85714f0a1c3b11160a
William Kinzig <wippawill2@gmail.com>
2018-05-30 18:45:42 -0700

    create initial calculator project setup. Directories and files.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(num1,num2) {
+	return num1 + num2;
+}
+
+var number1 = parseInt(prompt("Enter first number"));
+
+var number2 = parseInt(prompt("Enter second number"));
+
+alert(add(number1,number2));
