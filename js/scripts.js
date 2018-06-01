b22a7d764e91134628c40348232c2db8c9c65fea
William Kinzig <wippawill2@gmail.com>
2018-05-31 20:02:20 -0700

    add input labels, nested front-end code into documents ready

--- js/scripts.js
+++ js/scripts.js
@@ -23,11 +23,13 @@ var multiply = function(num1,num2)
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-
-	alert(add(number1,number2));
+  $("form#add").submit(function(event) {
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  alert(add(number1, number2));
+	$("#output").text(result);
 	alert(subtract(number1,number2));
 	alert(multiply(number1,number2));
 	alert(divide(number1,number2));
+});
 });
